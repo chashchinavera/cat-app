4c5cb28b23351ea7812a2f095c763d02776d7360
Vera Chashchina <chaschinavera@gmail.com>
2026-05-05 21:07:12 +0300

    fix: update fetch url

--- src/services/catService.ts
+++ src/services/catService.ts
@@ -1,14 +1,13 @@
 import { Cat } from "@/types/cat";
 
 const API_KEY = process.env.NEXT_PUBLIC_CAT_API_KEY || "";
-const BASE_URL = "https://api.thecatapi.com/v1";
 
 export const fetchCats = async (
   page: number,
   limit: number = 10,
 ): Promise<Cat[]> => {
   const res = await fetch(
-    `${BASE_URL}/images/search?limit=${limit}&page=${page}&order=ASC`,
+    `https://api.thecatapi.com/v1/images/search?limit=${limit}&page=${page}&order=ASC`,
     {
       headers: { "x-api-key": API_KEY },
     },
